50e5842b093e3a0f642792f9d3d8edaacbd6bc43
AnyaDroz <anya.drozdova89@gmail.com>
2024-06-22 16:44:47 +0100

    feat: add incremement by amount

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,5 +1,5 @@
 import {useDispatch, useSelector } from "react-redux";
-import {decrement, increment } from "./state/counter/counterSlice";
+import {decrement, increment, incrementByAmount } from "./state/counter/counterSlice";
 import {RootState} from "./state/store";
 
 const Counter = () => {
@@ -7,12 +7,15 @@ const Counter = () => {
     const count = useSelector((state: RootState) => state.counter.value)
     const dispatch = useDispatch()
 
+
+
     return(
         <div>
             <h2>{count}</h2>
             <div>
-                <button onClick={()=> dispatch(increment())}>increment</button>
+                <button onClick={() => dispatch(increment())}>increment</button>
                 <button onClick={() => dispatch(decrement())}>decrement</button>
+                <button onClick={() => dispatch(incrementByAmount(10) )}>incrementByAmount</button>
             </div>
         </div>
     )
